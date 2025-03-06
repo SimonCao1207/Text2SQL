d5dea5512eac6bfcbd139b48122a569fe887d34d
SimonCao1207 <hainamkaist@gmail.com>
2025-03-06 10:19:44 +0900

    sml fix

--- const.py
+++ const.py
@@ -1,20 +1,24 @@
 import os
 
-# Student ID
+# Team ID
 TEAM_ID = "12"
 
 # Directory paths for database, results and scoring program
 DB_ID = "mimic_iv"
 BASE_DATA_DIR = "data"
+EHRSQL_DATA_DIR = "EHRSQL_data"
 RESULT_DIR = "results"
 SCORING_DIR = "scoring"
 
-# File paths for the dataset and labels
-TABLES_PATH = os.path.join("database", "tables.json")  # JSON containing database schema
-VALID_DATA_PATH = os.path.join(
-    BASE_DATA_DIR, "valid_data.json"
-)  # JSON file for validation data
-VALID_LABEL_PATH = os.path.join(
-    BASE_DATA_DIR, "valid_label.json"
-)  # JSON file for validation labels (for evaluation)
-DB_PATH = os.path.join("data", DB_ID, f"{DB_ID}.sqlite")  # Database path
+TABLES_PATH = os.path.join("database", "tables.json")
+
+VALID_DATA_PATH = os.path.join(BASE_DATA_DIR, "valid_data.json")
+VALID_LABEL_PATH = os.path.join(BASE_DATA_DIR, "valid_label.json")
+
+# TRAIN_DATA_PATH = os.path.join(EHRSQL_DATA_DIR, "train", "data.json")
+# TRAIN_LABEL_PATH = os.path.join(BASE_DATA_DIR, "label", "label.json")
+
+# VALID_DATA_PATH = os.path.join(EHRSQL_DATA_DIR, "valid", "data.json")
+# VALID_LABEL_PATH = os.path.join(BASE_DATA_DIR, "label", "label.json")
+
+DB_PATH = os.path.join("database", DB_ID, f"{DB_ID}.sqlite")  # Database path
