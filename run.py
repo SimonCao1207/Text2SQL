d54b3df40d0f5c2bf50a51ae48e09ec373fe9f0f
SimonCao1207 <hainamkaist@gmail.com>
2025-03-20 14:53:53 +0900

    retrieve using faiss index

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
+import os  # Add this import
+
 from baseline import get_conversation
 from const import (
+    NULL_QUESTION_DATA_PATH,
+    NULL_QUESTION_INDEX_PATH,
     TEST_DATA_PATH,
 )
 from model import Model, post_process
-from retrieve import Retriever
+from retrieve import Retriever, VectorDB
 from utils import load_data, submit
 
 
@@ -15,16 +19,33 @@ def is_short_question(question):
 if __name__ == "__main__":
     test_data, _ = load_data(TEST_DATA_PATH, None, is_test=True)
     data = test_data["data"]
-    rv = Retriever()
+
+    if not os.path.exists("./data/index"):
+        os.makedirs("./data/index")
+
+    null_vector_db = VectorDB(
+        dataset_path=NULL_QUESTION_DATA_PATH, index_path=NULL_QUESTION_INDEX_PATH
+    )
+
+    if not null_vector_db.index_path.exists():
+        null_vector_db.build_index()
+    else:
+        null_vector_db.load_index()
+
+    null_retriever = Retriever(null_vector_db)
+    thres = 0.4
+
     myModel = Model()
     final_ret = {}
     for sample in data:
         str_id, question = sample["id"], sample["question"]
         if is_short_question(question):
             final_ret[str_id] = "null"  # Abstain
-        most_sim_id, most_sim_question, sim_score = rv.retrieve(question)
-        if sim_score > 0.75:
-            final_ret[str_id] = "null"  # Abstain
+        results = null_retriever.retrieve(question)
+        if results:
+            most_similar_question, distance = results[0]
+            if distance <= thres:
+                final_ret[str_id] = "null"  # Abstain
         else:
             prompt = get_conversation(question)
             answer, _ = myModel.ask_chatgpt(prompt)
